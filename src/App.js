fad393a2b259d8edb3780723bb84a51122b3aa06
Vinayak Singoriya <vinayaksingoriya14@gmail.com>
2023-08-20 16:24:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world Folks!!
+          Hello world from Appychip!!
         </p>
         <a
           className="App-link"
